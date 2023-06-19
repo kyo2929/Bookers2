4df2d9ba958b69b1e716fa0101128ed7842d5c95
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-06-19 09:54:14 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,23 @@
 class BooksController < ApplicationController
   def new
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to '/home'
+  end
 
   def index
   end
 
   def show
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @book = @user.post_images
   end
 
   def edit
+    @user = User.find(params[:id])
   end
-
-  def view
-  end
+  
 end
